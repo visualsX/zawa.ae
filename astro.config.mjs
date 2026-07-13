3f122e5d5f9e92521d58cb464d48ebb203103ab4
osamajavaid <iosamajavaid@gmail.com>
2026-07-13 08:13:38 +0500

    feat:seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
+  site: "https://zawa.ae",
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
